5c5058c4a8a0cb61de1b2c0e9e113991369665a4
simon y <71997308+kjai139@users.noreply.github.com>
2023-09-11 18:07:04 -0400

    start userauth

---
+++ frontend/src/app/_components/UserLogin.js
@@ -0,0 +1,24 @@
+const UserLogin = () => {
+    return (
+        <form className="flex flex-col gap-2">
+            <div className="input-cont">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" className="custom-input"></input>
+            </div>
+            <div className="input-cont">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" className="custom-input"></input>
+            </div>
+            <div className="flex justify-between">
+                <div>
+                    <button className="text-blue-500 p-1">Create a new account</button>
+                </div>
+                <div>
+                    <button className="text-blue-500 p-1">Login</button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default UserLogin
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+        mono: ['var(--font-roboto)']
+      },
+      fontSize: {
+        '2xl': '1.5rem',
+        '3xl': '2rem',
+        
+      },
+      boxShadow: {
+        'cont': 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;'
+      },
+      
+      
     },
   },
   plugins: [],
